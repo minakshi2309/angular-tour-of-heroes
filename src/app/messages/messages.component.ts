ebf4e328fd74c1416a05686d41e12329ea3864ee
minakshi2309 <minakshi.singh92@gmail.com>
2021-03-23 22:06:59 +0100

    1 file modified, 3 added

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+
+export class MessagesComponent {}
