6291bbdb5f69d022548751f6bb19c66f7cd23b68
minakshi2309 <minakshi.singh92@gmail.com>
2021-03-24 13:51:35 +0100

    5 files modified

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./dashboard.component.css']
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   heroes: Hero[]=[];
   constructor(private heroService: HeroService){}
   ngOnit(){
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,8 +11,8 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   //heroes = HEROES;
-  heroes: Hero[] = [];
-  selectedHero?: Hero;
+  heroes: Hero[];
+  //selectedHero?: Hero;
   constructor(private heroService: HeroService, private messageService: MessageService) {}
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
@@ -20,8 +20,8 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void {
+  /*onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  }*/
 }
