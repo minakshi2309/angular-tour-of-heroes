636607e8e88dba649a0dd8583791b3fcace8c051
minakshi2309 <minakshi.singh92@gmail.com>
2021-03-24 13:40:14 +0100

    3 files modified, 4 added

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from "../hero";
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+
+export class DashboardComponent {
+  heroes: Hero[]=[];
+  constructor(private heroService: HeroService){}
+  ngOnit(){
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1,5));
+  }
+}
