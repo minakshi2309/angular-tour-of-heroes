8ed07cdd743c5cbb93e570bbb2c4367ba33582cd
minakshi2309 <minakshi.singh92@gmail.com>
2021-03-24 09:58:03 +0100

    7 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,9 +22,3 @@ import { MessagesComponent } from './messages/messages.component';
 })
 export class AppModule { }
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
