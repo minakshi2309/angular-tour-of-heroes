2825858f38874e6cdf3eb353143b9e214f88a866
minakshi2309 <minakshi.singh92@gmail.com>
2021-03-24 18:33:24 +0100

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
